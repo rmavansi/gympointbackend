8d2b7a56a9d24e0de3791bb0ce74166b254befa6
Renan <rmavansi@gmail.com>
2019-12-03 20:59:55 -0300

    feature: initial commit, backend with users, students and its migrations and seeds

---
+++ backend/src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  secret: 'c0cd30b79a9aeza5f3975958a75392cd',
+  expiresIn: '7d',
+  login: '',
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import StudentController from './app/controllers/StudentController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
+
+export default routes;
